085a15f80543f82a653cd6c2eafcda6120b988ee
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2022-12-11 17:18:20 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,6 @@ int main(void)
 
     srand(time(0));
     n = rand() - RAND_MAX / 2;
-
     if (n == 0)
     {
         printf("%d is zero\n", n);
@@ -27,6 +26,5 @@ int main(void)
     {
         printf("%d is positive\n", n);
     }
-
     return (0);
 }
