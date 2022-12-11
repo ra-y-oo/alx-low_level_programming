5196f4b1d1ac68044fcbaaa6107663dce0a2b126
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2022-12-11 17:23:03 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,10 +11,12 @@ int main(void)
     int ch;
 
     for (ch = 'a'; ch <= 'z'; ch++)
+    {
         if (ch != 'e' && ch != 'q')
         {
             putchar(ch);
         }
+    }
     putchar('\n');
     return 0;
 }
