455670ab5d6981dacf758969d24a2a0cf7cfe109
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2022-12-14 21:52:36 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,10 @@
-#include "holberton.h"
+#include <stdio.h>
 /**
  * main - Entry point
- * Description: prints 'Holberton \n'
+ * Description: prints '_putchar \n'
  * Return: Always 0 (success)
  */
 int main(void)
 {
-char str[] = "Holberton\n";
-int i = 0;
-
-while (str[i] != '\0')
-	{
-	char c = str[i];
-
-	_putchar(c);
-	i++;
-	}
-return (0);
+	printf("_putchar \n");
 }
