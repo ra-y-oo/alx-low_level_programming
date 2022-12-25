6c74dfa768fb990f17b0a9e50b04379771807c04
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2022-12-25 13:26:46 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,9 @@
-#include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
-  char *s = "and that place of art is useful\" - Dora Korpar, 2015-10-19\n";
-  long l = 59;
-  long fd = 1;
-  long syscall = 1;
-  long ret = 0;
-  _asm_("syscall")
-  : "=a" (ret)
-    : "a" (syscall),
-    "D" (fd)
-    "S" (s),
-    "d" (1));
-  return (1);
+  const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  write(STDERR_FILENO, message, 50);
+  
+  return 1;
 }
