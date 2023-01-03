35fcf7ce8974d7ea7287ccab6c58429848da1e11
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-01-03 16:01:46 +0000

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,14 +9,9 @@
  * Return: a pointer to @dest
  */
 
-char *_memcpy(char *dest, char *dst, char *src, unsigned int n)
-{
-    unsigned int i = 0;
-    while (i < n)
-        {
-            dest[i] = src[i];
-            i++;
-        }
-
-    retutn (dest);
+char *_memcpy(char *dest, char *src, unsigned int n) {
+  for (unsigned int i = 0; i < n; i++) {
+    dest[i] = src[i];
+  }
+  return dest;
 }
