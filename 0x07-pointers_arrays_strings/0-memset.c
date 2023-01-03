a301a04c14dab3085c8f651b83d1463fb8bb3a3f
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-01-03 16:28:47 +0000

    Update 0-memcpy.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,15 +3,24 @@
 /**
  * _memset - function fill the first @n bytes of the memory area pointed
  * to by @s with the constant byte @b
+ *
  * @n: bytes of the memory area pointed to by @s
+ *
  * @s: with the constant byte @b
+ *
  * @b: memory area pointer
+ *
  * Return: a pointer to the memory area @s
  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
 
-char *_memset(char *s, char b, unsigned int n) {
-  for (unsigned int i = 0; i < n; i++) {
-    s[i] = b;
-  }
-  return s;
+	while (i < n)
+	{
+	s[i] = b;
+	i++;
+	}
+
+	return (s);
 }
