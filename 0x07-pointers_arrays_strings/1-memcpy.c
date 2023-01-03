84dbf24a2d25d242300f6496bf91807ca88d030f
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-01-03 16:29:54 +0000

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,15 +3,24 @@
 /**
  * _memcpy - function copies @n bytes from memory area @src
  * to memory area @dest
+ *
  * @n: function copies
+ *
  * @src: bytes from memory area
+ *
  * @dest: to memory area
+ *
  * Return: a pointer to @dest
  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
 
-char *_memcpy(char *dest, char *src, unsigned int n) {
-  for (unsigned int i = 0; i < n; i++) {
-    dest[i] = src[i];
-  }
-  return dest;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
 }
