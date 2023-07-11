1a83c0fde155bb7ddc5d70cee2fee634905c08aa
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-07-11 21:57:43 +0300

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <math.h>
 #include "search_algos.h"
 
 int jump_search(int *array, size_t size, int value){
-    
+    int i, start=0, end=sqrt(size);
+    while(array[end] < value && start < size){
+        start=end;
+        end=end + sqrt(size);
+        if(end > size-1)
+            end=size;
+    }
+    for(i=start; i<end; i++){
+        if(array[i] == value)
+            return i;
+    }
+    return -1;
 }
