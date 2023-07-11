d0bbebc2164fe1268bb1a68532ffaff291edb3de
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-07-11 22:39:14 +0300

    Add comment jumpsearch

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 #include "search_algos.h"
+/**
+ * jump_search -searches for a value in a sorted array of integers using the Jump search algorithm
+ */
 
 int jump_search(int *array, size_t size, int value){
+    //initialize the start and end of the sub arrays
     int i, start=0, end=sqrt(size);
+    //Perform the jump search
     while(array[end] < value && start < size){
         start=end;
         end=end + sqrt(size);
         if(end > size-1)
             end=size;
     }
+    //perform Linear search for the jump section
     for(i=start; i<end; i++){
         if(array[i] == value)
             return i;
